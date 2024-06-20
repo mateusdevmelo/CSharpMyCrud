d3d7baaa0da6b83444a63e2c58c1fe32c9c978d7
Mateus Melo <mateusdevmelo@gmail.com>
2024-06-19 23:17:46 -0300

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyCrudApi.Models;
+
+namespace MyCrudApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {           
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
